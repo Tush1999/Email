ba5d1c7c4c1d0a58755c0bf7a133fc1c23f3cf86
Tushar Bansal <tusharbansal104@gmail.com>
2021-09-29 17:58:53 +0530

    Email Slots

--- src/Components/Email/index.js
+++ src/Components/Email/index.js
@@ -1,20 +1,23 @@
 import React, { Component } from "react";
+import data from "../../Content";
 
 export default class Email extends Component {
-    render(){
-        return(
-            <>
-            <div className="content">
-            <div>{this.props.Saluation} {this.props.ReceiverName},</div>
-            <div className="inner-content">
-            {this.props.emailContent}
-            </div>
-            <div className="end-content">
-            <div>{this.props.Named},</div>
+  static defaultProps = {
+    ReceiverName: "Sir",
+    SenderName: "Anonymous",
+  };
+  render() {
+    return (
+      <>
+        <div className="content">
+          <div>Dear {this.props.ReceiverName},</div>
+          <div className="inner-content">{data}</div>
+          <div className="end-content">
+            <div>Thanks,</div>
             <div>{this.props.SenderName}</div>
-            </div>
-            </div>
-            </>
-        )
-    }
+          </div>
+        </div>
+      </>
+    );
+  }
 }
--- src/Components/EmailBody/index.js
+++ src/Components/EmailBody/index.js
@@ -1,27 +1,17 @@
 import React, { Component } from "react";
 import Email from "../Email/index";
-import data from "../../Content";
+
 import "./style.css";
 
 export default class EmailBody extends Component {
-  static defaultProps = {
-    ReceiverName: "Sir",
-    SenderName: "Anonymous",
-    Saluation: "Hi",
-    Named: "Regards",
-  };
-
   render() {
     return (
       <React.Fragment>
         <div className="header">
-          <Email
-            ReceiverName={this.props.ReceiverName}
-            SenderName={this.props.SenderName}
-            emailContent={data}
-            Saluation={this.props.Saluation}
-            Named={this.props.Named}
-          />
+          <Email />
+        </div>
+        <div className="header">
+          <Email ReceiverName={"Sawan"} SenderName={"Tushar"} />
         </div>
       </React.Fragment>
     );
