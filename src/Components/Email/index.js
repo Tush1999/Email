8a12db13a47f10a58d14e729f8cd1f375251208b
Tushar Bansal <tusharbansal104@gmail.com>
2021-10-05 13:06:45 +0530

    Add minor changes

--- src/Components/Email/index.js
+++ src/Components/Email/index.js
@@ -2,21 +2,17 @@ import React, { Component } from "react";
 import data from "../../Content";
 
 export default class Email extends Component {
-  static defaultProps = {
-    ReceiverName: "Sir",
-    SenderName: "Anonymous",
-  };
   render() {
+    let datas = this.props.ReceiverName
+      ? data.replace("Sir", this.props.ReceiverName)
+      : data;
+    datas = this.props.SenderName
+      ? datas.replace("Anonymous", this.props.SenderName)
+      : datas;
+    let text = datas.split("\n").map((i) => <p>{i}</p>);
     return (
       <>
-        <div className="content">
-          <div>Dear {this.props.ReceiverName},</div>
-          <div className="inner-content">{data}</div>
-          <div className="end-content">
-            <div>Thanks,</div>
-            <div>{this.props.SenderName}</div>
-          </div>
-        </div>
+        <div>{text}</div>
       </>
     );
   }
